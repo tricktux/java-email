4e661fb4583f4beaf3cd567cc2227001cdb77c12
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2019-06-30 10:04:49 -0400

    New test for factory

--- src/test/java/com/molina/emails/LibraryTest.java
+++ src/test/java/com/molina/emails/LibraryTest.java
@@ -9,8 +9,9 @@ import static org.junit.Assert.*;
 public class LibraryTest {
   @Test
   public void testSomeLibraryMethod() {
-    Library classUnderTest = new Library();
-    assertTrue("someLibraryMethod should return 'true'", 
-				classUnderTest.someLibraryMethod());
+    // Library classUnderTest = new Library();
+    // assertTrue("someLibraryMethod should return 'true'",
+				// classUnderTest.someLibraryMethod());
+		Library classUnderTest = Library.LibraryFactory("../../email_config.ini");
   }
 }
