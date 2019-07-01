b1d4c5ac3944accaa0da49bd65aa3910339a85fb
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2019-06-30 22:08:56 -0400

    Fix logging ints

--- src/main/java/com/molina/emails/EmailBuilder.java
+++ src/main/java/com/molina/emails/EmailBuilder.java
@@ -12,48 +12,53 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
 class EmailBuilder {
-	final static Log logger = LogFactory.getLog(EmailBuilder.class);
+  final static Log logger = LogFactory.getLog(EmailBuilder.class);
 
-	public Email build(String configFile) {
-		if (configFile.isEmpty()) {
-			logger.error("Invalid function input");
-			return null;
-		}
+  public Email build(String configFile) {
+    if (configFile.isEmpty()) {
+      logger.error("Invalid function input");
+      return null;
+    }
 
 		logger.debug(String.format("Config file = '%s'", configFile));
-		try {
-			Wini ini = new Wini(new File(configFile));
-
-			String from = ini.get("email", "from");
-			logger.debug(String.format("from = '%s'", from));
-			String host = ini.get("email", "host");
-			logger.debug(String.format("host = '%s'", host));
-			String port = ini.get("email", "port");
-			logger.debug(String.format("port = '%s'", port));
-			int ssl = ini.get("email", "ssl", int.class);
-			logger.debug(String.format("ssl = '%i'", ssl));
-			int tls = ini.get("email", "tls", int.class);
-			logger.debug(String.format("tls = '%i'", tls));
-
-			String username = ini.get("user", "username");
-			logger.debug(String.format("username = '%s'", username));
-			String password = ini.get("user", "password");
-			logger.debug(String.format("password = '%s'", password));
-
-			Email email = new Email();
-			email.setFrom(from);
-			email.setHost(host);
-			email.setPort(port);
-			email.setSsl(ssl);
-			email.setTls(tls);
-			email.setUsername(username);
-			email.setPassword(password);
-
-			return email;
-		} catch(Exception e) {
-			logger.trace("Exception accessing config file");
+    File f = new File(configFile);
+    if(!f.exists() || f.isDirectory()) {
+			logger.error("Provided file does not exists");
 			return null;
-		}
-	}
+    }
+    try {
+      Wini ini = new Wini(f);
+
+      String from = ini.get("email", "from");
+      logger.debug(String.format("from = '%s'", from));
+      String host = ini.get("email", "host");
+      logger.debug(String.format("host = '%s'", host));
+      String port = ini.get("email", "port");
+      logger.debug(String.format("port = '%s'", port));
+      int ssl = ini.get("email", "ssl", int.class);
+      logger.debug(String.format("ssl = '%d'", ssl));
+      int tls = ini.get("email", "tls", int.class);
+      logger.debug(String.format("tls = '%d'", tls));
+
+      String username = ini.get("user", "username");
+      logger.debug(String.format("username = '%s'", username));
+      String password = ini.get("user", "password");
+      logger.debug(String.format("password = '%s'", password));
+
+      Email email = new Email();
+      email.setFrom(from);
+      email.setHost(host);
+      email.setPort(port);
+      email.setSsl(ssl);
+      email.setTls(tls);
+      email.setUsername(username);
+      email.setPassword(password);
+
+      return email;
+    } catch(Exception e) {
+      logger.trace("Exception accessing config file");
+      return null;
+    }
+  }
 
 }
